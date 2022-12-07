95865185fd8afcde17ebb46dd1c0fc8fa2980c07
Krishan Shamod <krishanshamod@gmail.com>
2022-12-07 19:57:39 +0530

    Updated the view model

--- app/src/main/java/com/krishanshamod/simple_android_application_7/ui/main/MainViewModel.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_7/ui/main/MainViewModel.kt
@@ -1,7 +1,18 @@
 package com.krishanshamod.simple_android_application_7.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var _text = MutableLiveData<String>().apply {
+        value = ""
+    }
+
+    val text: MutableLiveData<String> = _text
+
+    fun updateText(text: String) {
+        _text.value = text
+    }
+
 }
